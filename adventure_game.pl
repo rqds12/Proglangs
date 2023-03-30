519f3e46711e71b8b56cd1a83b0034ef428dd5f6
Seth Kroeker <sethkroeker@letu.edu>
2023-03-29 23:38:22 -0500

    finished typing inital parsing

--- adventure_game.pl
+++ adventure_game.pl
@@ -9,15 +9,16 @@ equal(X,Y):-
     X=Y.
 read_in :-
     write('enter the game data'),
-    read(user_input, "\n", "\t", End, Input), split_string(Input, " ", " ", Line),
+    read(user_input, "\n", "\t", End, Input), split_string(Input, ",", ",", Line),
     process(Line).
 
-process(Type|Line):-
+process([Head|Rest]):- process_first([Head],[Rest])
+process_first([Format|L],[Rest]):-
     switch(Type, [
-               equal(Type,"relation"): (convert_stringLIst_atomList(Line,AtomList),parse_relation(AtomList)),
-               equal(Tdype, "group"):  ,
-               equal(Type, "noun"):,
-               equal(Type, "end"):  %change state to the repl
+               equal(Format,"relation"): (convert,_stringList_atomList(Rest,AtomRest),(convert_stringList_atomList(L,AtomList),parse_relation(AtomRest, AtomList))),
+               equal(Format, "group"): (convert_stringList_atomList(L,AtomList), parse_group(AtomList)) ,
+               equal(Format, "noun"):, (convert_stringList_atomList(L,AtomList), parse_noun(AtomList)),
+               equal(Format, "end"):  %change state to the repl
            ]).
 
 
@@ -34,17 +35,14 @@ assert_noun(Atom, [Hatom|Rest]):-
     assert(Atom(Hatom)), assert_noun(Atom, [Rest]).
 assert_noun(Atom,[]).
 
-
-%relation dcg
-relation --> b,g.
-b --> [T].
-g --> [c], g.
-g --> [c]
-
-parse_relation(Atom|Line):-
-    
+parse_relation([R|H2|L2],[H1|L1]):- assert(R(H1,H2)), assert_relation(H1,[L1]), assert_relation(H2,[L2]).
+assert_relation(Atom, [Type|Rest]):-
+    assert(Atom(Type)), assert_relation(Atom,[Rest]).
+assert_relation(Atom,[]).
 
 
 convert_stringList_atomList(L,R) :- accConv(L,R).
 accConv([],[]).
 accConv([H|L1], [atom_string(X,H),L2]) :-  accConv(L1,L2).
+
+
