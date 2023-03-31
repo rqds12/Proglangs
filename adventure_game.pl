f8ec10856c93dc234d410571d510c16acd2de1fc
Seth Kroeker <sethkroeker@letu.edu>
2023-03-31 08:59:22 -0500

    figured out the conversion to atom finally jeez am i stoopid

--- adventure_game.pl
+++ adventure_game.pl
@@ -1,7 +1,8 @@
 :- dynamic(relation/3).
 :- dynamic(group/2).
 :- dynamic(noun/2).
-
+:- dynamic(currentRoom/1).
+:- dynamic(inventory/1).
 
 
 switch(X, [Val: Goal|Cases]) :-
@@ -50,7 +51,7 @@ parse_relation2([H2|L2],[H1|L1], R):- assert(relation(R,H1,H2)), assert_noun(H1,
 
 convert_stringList_atomList(L,R) :- accConv(L,R).
 accConv([],[]).
-accConv([H|L1], [atom_string(X,H),L2]) :-  accConv(L1,[X|L2]).
+accConv([H|L1], [H2|L2]) :-  accConv(L1,L2), atom_string(H2,H).
 
 
 value_of_var(Var):-Var.
@@ -68,10 +69,41 @@ move: moves the player. Eg: move room1.\n
 pick_up: picks up an object. Eg: pick_up chair1.\n
 place: places and object down. Eg: place chair1.\n
 look: lists the ‘objects’ present in a room.\n
-inventory: displays the inventory of the player. Eg: inventory.\n"), write_list(actions()).
+inventory: displays the inventory of the player. Eg: inventory.\n"), write_list('~w:~n',actions()).
 
-write_list([H|Rest]):-
-    format('~w:~n', [H]), write_list(Rest).
+write_list(Str, [H|Rest]):-
+    format(Str, [H]), write_list(Rest).
 write_list([]). %base case.
 
+%parse repl  X is a string.  Out is a
+%parse_repl(X, Out):-.
 
+%move checks if the current room is connected to the desired room. it reasserts the current room
+move(Y):-
+    relation(connected, currentRoom(X), Y) ->
+        (retract(currentRoom(X)),assert(currentRoom(Y))).
+
+%adds an element to the inventory
+pick_up(X):-
+    assert(inventory(X)).
+
+%removes an element from the inventory
+place(X):-
+    inventory(X) ->
+        retract(inventory(X)).
+
+inventory():-
+    setof(X,inventory(X),L), write_list('~w:~n',L).
+
+look():- setof(X, noun(X,_), L), write_list('You see a ~w ~n',L).
+
+
+
+%todo for seth
+%add text output for move, pickup, place
+%add generic action output
+%write repl
+%when evaluating
+%you can compare the first word the action, against all actions.  if it is in there, do the generic case
+%otherwise try to call the specific ones
+%
